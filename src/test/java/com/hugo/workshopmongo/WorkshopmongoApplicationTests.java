323bc8c6a35586f3cd264e811cfff299154316eb
HuugoAlexandre <huugoalexanndre@gmail.com>
2025-02-23 15:40:29 -0300

    First commit - project created

---
+++ src/test/java/com/hugo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
